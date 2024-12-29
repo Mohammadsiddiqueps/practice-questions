b2787995f02a7d3a1faf292aaad6deb918211ea5
Mohammadsiddiqueps <126324832+Mohammadsiddiqueps@users.noreply.github.com>
2024-12-29 14:38:23 +0530

    done merge objects

--- reduce.js
+++ reduce.js
@@ -89,12 +89,15 @@ const countVowelsInWords = function (words) {
 };
 
 // makeCamelCase(["hello", "world", "how", "are", "you"]) => "helloWorldHowAreYou"
+const capitalize = (string) => {
+  const array = [...string];
+  array[0] = array[0].toUpperCase();
+
+  return array.join("");
+};
+
 const makeCamelCase = function (words) {
-  return words.reduce((a, b) => {
-    const array = [...b];
-    array[0] = array[0].toUpperCase();
-    return a + array.join("");
-  });
+  return words.reduce((camelWord, element) => camelWord + capitalize(element));
 };
 
 // reverseString(["apple", "banana", "cherry"]) => "elppaananabyrrehc"
@@ -114,22 +117,51 @@ const concatenateArrays = function (arrays) {
 
 // flattenArray([[1, 2], [3, 4], [5, 6]]) => [1, 2, 3, 4, 5, 6]
 const flattenArray = function (arrays) {
-  //
+  return arrays.reduce((a, b) => {
+    if (Array.isArray(b)) {
+      return [...a, ...flattenArray(b)];
+    }
+
+    return [...a, b];
+  }, []);
 };
 
 // uniqueNumbers([1, 2, 2, 3, 4, 4, 5]) => [1, 2, 3, 4, 5]
 const uniqueNumbers = function (numbers) {
-  //
+  return numbers.reduce((a, b) => (a.includes(b) ? a : [...a, b]), []);
 };
 
 // groupByLength(["apple", "banana", "cherry", "date"]) => { 5: ["apple", "cherry"], 6: ["banana"], 4: ["date"] }
-const groupByLength = function (strings) {};
+const groupByLength = function (strings) {
+  return strings.reduce((groupedStrings, element) => {
+    if (!(element.length in groupedStrings)) {
+      groupedStrings[element.length] = [];
+    }
+
+    groupedStrings[element.length].push(element);
+    return groupedStrings;
+  }, {});
+};
 
 // countOccurrences(["apple", "banana", "cherry", "banana"]) => { apple: 1, banana: 2, cherry: 1 }
-const countOccurrences = function (strings) {};
+const countOccurrences = function (strings) {
+  return strings.reduce((a, b) => {
+    return transformKey(a, b, 0, (count) => count + 1);
+  }, {});
+};
 
 // mergeObjects([{ a: 1, b: 2 }, { b: 3, c: 4 }, { a: 5 }]) => { a: 6, b: 5, c: 4 }
-const mergeObjects = function (objects) {};
+
+const merge = (mergedObj, object) => {
+  return Object.keys(object).reduce(
+    (groupedObj, key) =>
+      transformKey(groupedObj, key, 0, (value) => value + object[key]), mergedObj
+  );
+};
+
+const mergeObjects = function (objects) {
+  return objects.reduce(merge, {});
+};
 
 // zip(["a", "b", "c"], [1, 2, 3]) => { "a": 1, "b": 2, "c": 3 }
 const zip = function (keys, values) {};
@@ -218,13 +250,69 @@ const findMinMax = function (numbers) {
 };
 
 // sumByCategory([{ category: 'A', value: 10 }, { category: 'B', value: 20 }, { category: 'A', value: 30 }]) => { A: 40, B: 20 }
-const sumByCategory = function (items) {
-  return items.reduce((sumOfCategory, element) => {
-    if (!Object.keys(sumOfCategory).includes(element.category)) {
-      sumOfCategory[element.category] = 0;
-    }
+// const sumByCategory = function (items) {
+//   return items.reduce((sumOfCategory, element) => {
+//     if (!(element.category in sumOfCategory)) {
+//       sumOfCategory[element.category] = 0;
+//     }
 
-    sumOfCategory[element.category] += element.value;
-    return { ...sumOfCategory };
-  }, {});
+//     sumOfCategory[element.category] += element.value;
+//     return sumOfCategory;
+//   }, {});
+// };
+
+// const words = ["apple", "banana", "grape"];
+
+// const isVowel = (char) => "aeiou".includes(char.toLowerCase());
+
+// words.reduce((vowelCounts, word) => {
+//   word
+//     .split("")
+//     .filter(isVowel)
+//     .reduce((vowelCounts, vowel) => {
+//       transformKey(vowelCounts, vowel, 0, (existingCount) => existingCount + 1);
+//       return vowelCounts;
+//     }, vowelCounts);
+//   return vowelCounts;
+// }, {});
+
+const transformKey = (object, key, defaultValue, transformer) => {
+  if (!(key in object)) {
+    object[key] = defaultValue;
+  }
+  const value = object[key];
+  object[key] = transformer(value);
+  return object;
 };
+
+// const sumByCategory = function (items) {
+//   return items.reduce((sumOfCategory, element) => {
+//     insertIfNotPresent(sumOfCategory, element.category, 0);
+//     sumOfCategory[element.category] += element.value;
+
+//     return sumOfCategory;
+//   }, {});
+// };
+
+// const array = ["apple", "banana", "cherry", "date"];
+
+array.reduce(
+  (groups, element) =>
+    transformKey(groups, element.length, [], (existingGroup) => [
+      ...existingGroup,
+      element,
+    ]),
+  {}
+);
+
+function groupBy(list, keyFN) {
+  return list.reduce((group, element) => {
+    const objectKey = keyFN(element);
+    transformKey(group, objectKey, [], array);
+    insertIfNotPresent(group, objectKey, []);
+    group[objectKey].push(element);
+    return group;
+  }, {});
+}
+
+console.log(groupBy(array, length));
